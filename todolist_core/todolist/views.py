0c8509007b619431ecf2563884a78e67c36313b4
Mihail Genzik <genziks@inbox.lv>
2024-12-02 01:35:11 +0200

    Проверка на владельца задачи перед изменением задачи

--- todolist_core/todolist/views.py
+++ todolist_core/todolist/views.py
@@ -7,7 +7,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.generics import ListAPIView
 from .serializers import TagSerializer
-
+from users.permissions import CanEdit
 
 def base(request):
     return render(request, 'todolist/base.html')
@@ -31,6 +31,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class TodolistViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [CanEdit]
 
 
 class CommentListCreateAPIView(generics.ListCreateAPIView):
